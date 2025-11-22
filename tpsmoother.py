83d2e9393f5da994fd18c613a849807602ec96d6
Kenny <keeg@itu.dk>
2025-11-22 13:57:37 +0100

    Add basic absolute smoothing support
    Working for single tap, multitouch is work in progress

--- tpsmoother.py
+++ tpsmoother.py
@@ -11,7 +11,7 @@ parser.add_argument(
     "-d",
     "--device",
     help="Input device to use. (e.g. /dev/input/event1)",
-    required=True
+    required=True,
 )
 parser.add_argument(
     "-g",
@@ -31,29 +31,99 @@ parser.add_argument(
     "--min-frequency",
     type=int,
     default=30,
-    help="Minimum smoothing frequency. When real input frequency is below this value, the smoothing timer is forcibly set based on the specified minimum frequency."
+    help="Minimum smoothing frequency. When real input frequency is below this value, the smoothing timer is forcibly set based on the specified minimum frequency.",
 )
 parser.add_argument(
-    "-v",
-    "--verbose",
-    action="store_true",
-    help="Log verbose output in terminal."
+    "-v", "--verbose", action="store_true", help="Log verbose output in terminal."
 )
 
-def gen_rel_events(queue: queue.Queue[events.RelEvent], multiplier: int) -> tuple[list[events.RelEvent]]:
+
+def gen_rel_events(
+    queue: queue.Queue[events.RelEvent], multiplier: int
+) -> tuple[list[events.RelEvent]]:
     event_list = []
     while queue.qsize() != 0:
         event = queue.get()
-        event_list.append(event)
+        event_list.append(events.InputEvent(None, None, event.type, event.code, event.value))
 
     out = (event_list,) * multiplier
+    return out
+
+
+ABS_SMOOTHING_WHITELIST = [
+    ecodes.ABS_MT_POSITION_X,
+    ecodes.ABS_MT_POSITION_Y,
+    ecodes.ABS_MT_PRESSURE,
+    ecodes.ABS_PRESSURE,
+    ecodes.ABS_X,
+    ecodes.ABS_Y,
+]
+
+last_abs_events: dict[int, events.AbsEvent] = dict()
+
+def lerp(x: int, y: int, weight: float):
+    return x * (1 - weight) + y * weight
+
+def gen_abs_events(
+    queue: queue.Queue[events.AbsEvent], multiplier: int
+) -> tuple[list[events.AbsEvent]]:
+
+    # Process initial event
+    initial_events = []
+    
+    initial_smoothable_events = dict()
+    final_smoothable_events = dict()
+    
+    while queue.qsize() != 0:
+        event = queue.get()
+        event_initial_copy = events.InputEvent(None, None, event.type, event.code, event.value)
+        event_final_copy = events.InputEvent(None, None, event.type, event.code, event.value)
+        initial_events.append(event_initial_copy)
+        
+        if event.code == ecodes.ABS_MT_TRACKING_ID and event.value == -1:
+            last_abs_events.clear();
+        
+        if event.code in ABS_SMOOTHING_WHITELIST:
+            initial_smoothable_events[event.code] = event_initial_copy
+            final_smoothable_events[event.code] = event_final_copy
+
+    out = (initial_events,) + tuple([list() for _ in range(multiplier-1)])
+    
+    # Generate initial smoothed event
+    for event in initial_smoothable_events.values():
+        last_abs_event = last_abs_events.get(event.code)
+        last_abs_events[event.code] = event
+        if last_abs_event is None:
+            final_smoothable_events.pop(event.code)
+        else:
+            original_value = event.value
+            event.value = int(lerp(last_abs_event.value, original_value, 1/multiplier))
+            
+            # Intermediate smoothing events
+            for i in range(1, multiplier-1):
+                i_value = int(lerp(last_abs_event.value, original_value, (i+1)/multiplier))
+                i_event = events.InputEvent(None, None, event.type, event.code, i_value)
+                out[i].append(i_event)
+            
+    
+    # Final smoothed event
+    for event in final_smoothable_events.values():
+        out[multiplier - 1].append(event)
+        
+    # Print virtual events (for testing)
+    # i = 0
+    # for evs in out:
+    #     print(f"Virtual event {i}:")
+    #     i += 1
+    #     for event in evs:
+    #         print(f"  {event_str(event)}")
     return out
 
 
 def get_capabilities_str(device: InputDevice):
     caps = device.capabilities()
     ecode = lambda t, c: ecodes.bytype[t][c]
-    
+
     out = []
 
     for etype, codes in caps.items():
@@ -65,13 +135,23 @@ def get_capabilities_str(device: InputDevice):
                 out.append(f"    {(ecode(etype, code[0]), *code[1:])}")
             else:
                 out.append(f"    {code}")
-                
-    return '\n'.join(out)
+
+    return "\n".join(out)
+
 
 verbose = False
+
+
 def logv(*values):
     if verbose:
         print(*values)
+
+
+def event_str(event: events.InputEvent):
+    return (
+        f"{ecodes.EV[event.type]} {ecodes.bytype[event.type][event.code]} {event.value}"
+    )
+
 
 def main(args):
     device = InputDevice(args.device)
@@ -79,7 +159,7 @@ def main(args):
     print(f"Using device {args.device} - {device.name}")
     logv("Supported capabilities:")
     logv(get_capabilities_str(device))
-    
+
     print(f"Smoothing with {args.multiplier}x multiplier.")
 
     # Create virtual device
@@ -93,16 +173,12 @@ def main(args):
     rel_queue = queue.Queue()
     abs_queue = queue.Queue()
     event_queue = queue.Queue()
-    
+
     last_counter = None
-    
+
     logv("Verbose output specified, input events will be logged in terminal.")
     for event in device.read_loop():
-        logv(
-            ecodes.EV[event.type],
-            ecodes.bytype[event.type][event.code],
-            event.value,
-        )
+        logv(event_str(event))
         if event.type == ecodes.EV_REL:
             rel_queue.put(event)
         elif event.type == ecodes.EV_ABS:
@@ -112,18 +188,23 @@ def main(args):
 
         if event.type == ecodes.EV_SYN:
             rel_smoothed_events = gen_rel_events(rel_queue, args.multiplier)
-            
+            abs_smoothed_events = gen_abs_events(abs_queue, args.multiplier)
+
             for rel_event in rel_smoothed_events[0]:
                 ui.write(rel_event.type, rel_event.code, rel_event.value)
+
+            for abs_event in abs_smoothed_events[0]:
+                ui.write(abs_event.type, abs_event.code, abs_event.value)
 
             qevent = event_queue.get()
             while qevent.type != ecodes.EV_SYN:
                 ui.write(qevent.type, qevent.code, qevent.value)
                 qevent = event_queue.get()
-            
+
             ui.syn()
             logv(f"-- SYN ({ecodes.bytype[event.type][event.code]}) --")
-                
+
+            # After original event, send smoothed events
             frequency: int
             counter = time.perf_counter()
             if last_counter is None:
@@ -131,23 +212,26 @@ def main(args):
             else:
                 delta = time.perf_counter() - last_counter
                 frequency = 1 / delta
-                
+
             last_counter = counter
             logv(frequency)
-                
+
             if frequency < args.min_frequency:
                 frequency = args.min_frequency
             delay = 1 / (frequency * args.multiplier)
             for i in range(1, args.multiplier):
                 time.sleep(delay)
-                
+
                 for rel_event in rel_smoothed_events[i]:
                     ui.write(rel_event.type, rel_event.code, rel_event.value)
-                
+
+                for abs_event in abs_smoothed_events[i]:
+                    ui.write(abs_event.type, abs_event.code, abs_event.value)
+
                 ui.syn()
 
 
 if __name__ == "__main__":
-    args=parser.parse_args()
+    args = parser.parse_args()
     verbose = args.verbose
     main(args)
